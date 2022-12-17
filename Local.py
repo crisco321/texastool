89bd9228845accf4b14565dd6b46e38369e8ebcf
crisco321 <75357932+crisco321@users.noreply.github.com>
2022-12-17 03:38:26 -0500

    Create Local.py

---
+++ Local.py
@@ -0,0 +1,21 @@
+
+import smtplib
+
+from email.message import EmailMessage
+
+msg = EmailMessage()
+msg['Subject'] = 'Hello Python World!'
+msg['From'] = 'J. B. Goode <jgoode@example.com>'
+msg['To'] = 'Pump King <peter.pumpking@example.com>'
+msg.set_content("""Hello World,
+    I am writing to tell you that I am alive!
+
+Yours very truly,
+Johnny B. Goode
+""")
+
+smtp = smtplib.SMTP('localhost:8025')
+smtp.send_message(msg)
+smtp.quit()
+
+print(msg)
